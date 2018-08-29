fc0ff574d6e1be271165d37724e15a64bb887b1c
Nishant Singh <nishant.singh@somaiya.edu>
2018-08-29 09:07:39 +0530

    h2 database table

--- marketbackend/src/main/java/nit/sin/marketbackend/dto/Category.java
+++ marketbackend/src/main/java/nit/sin/marketbackend/dto/Category.java
@@ -1,5 +1,11 @@
 package nit.sin.marketbackend.dto;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Category {
 
 	public int getId() {
@@ -37,10 +43,16 @@ public class Category {
 	/*
 	 * Private fields
 	 * */
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
 	private String name;
+	
 	private String description;
+	
 	private String imageUrl;
+	
 	private boolean active = true;
 
 }
